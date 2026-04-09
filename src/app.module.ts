a610d86fb697d75169ebe0bf694545f603827d8d
ebraimsambo <ebraimsambo@gmail.com>
2026-04-09 12:13:52 +0100

    fix: rename folder module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './modules/user/user.module';
+import { UserModule } from './features/user/user.module';
 
 @Module({
   imports: [UserModule],
